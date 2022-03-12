671eef05bf9732db407fbbcffb133173affea86c
LesterCamp234 <59652859+LesterCamp234@users.noreply.github.com>
2022-03-12 10:21:05 +0100

    Update es 23 p.142.cpp

--- es 23 p.142.cpp
+++ es 23 p.142.cpp
@@ -2,22 +2,15 @@
 using namespace std;
 
 int main(){
-    int voto1 = 0, voto2 = 0, min = 0;
-    while (voto1 <= 0 && voto2 <= 0) {
-    cout << "Dammi il primo voto e il secondo voto \n";
-    cin >> voto1 >> voto2;
-    }
-    if (voto1 > voto2){
-        min = voto2;
-    } else {
-        min = voto1;
-    }
-    for (size_t i = 1; i <= 3; i++)
+    int voto = 0, min = 0;
+    cout << "Dammi un voto \n";
+    cin >> min;
+    for (size_t i = 1; i <= 4; i++)
     {
-        cout << "Dammi un altro voto \n";
-        cin >> voto2;
-        if (min > voto2){
-            min = voto2;
+        cout << "Dammi un voto \n";
+        cin >> voto;
+        if (min > voto){
+            min = voto;
             }
     }
     cout << "Il voto più basso è: " << min << endl;
